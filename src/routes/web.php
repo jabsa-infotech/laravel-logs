7e70b55a9aea723ec87274751fc74c9daf268b9b
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2025-04-08 16:40:53 +0530

    Refactor routes to use LogController for index and destroy actions

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Jabsa\LaravelLogs\Http\Controllers\LogController;
 
 Route::group([
     'prefix' => config('laravel-logs.route_prefix'),
+    'as' => config('laravel-logs.route_name_prefix') . '.',
     'middleware' => 'web'
 ], function(){
-    Route::get('/', function () {
-        return view('laravel-logs::hello');
-    });
+    Route::get('/', [LogController::class, 'index'])->name('index');
+    Route::delete('/destroy', [LogController::class, 'destroy'])->name('destroy');
 });
